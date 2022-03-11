256b37a04f7535534d91f16ef5de0311cb421afe
RayenBourguiba <58385581+RayenBourguiba@users.noreply.github.com>
2022-03-11 06:41:10 +0100

    Version 1.2.8

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use App\Repository\UserRepository;
+use App\Repository\CommandeRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +15,13 @@ class DashboardController extends AbstractController
     /**
     * @Route("/dashboard", name="dashboard")
     */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, CommandeRepository $commandeRepository, ProduitRepository $produitRepository): Response
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'users' => $userRepository->findBy([ 'banned' => '1']),
-
+            'commandes' => $commandeRepository->findAll(),
+            'produits' => $produitRepository->findBy(['active' => 'true']),
         ]);
     }
 }
