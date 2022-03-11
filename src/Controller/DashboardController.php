11ad37a9642fc02ad40aac046ea3ad971b9e2c17
ChawkiFerroukhi <75191392+ChawkiFerroukhi@users.noreply.github.com>
2022-03-11 06:34:11 +0100

    Version 1.2.7

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class DashboardController extends AbstractController
     /**
     * @Route("/dashboard", name="dashboard")
     */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'users' => $userRepository->findBy([ 'banned' => '1']),
+
         ]);
     }
 }
