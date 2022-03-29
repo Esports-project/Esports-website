b33067b8555982267b318b11255a57803202320b
RayenBourguiba <58385581+RayenBourguiba@users.noreply.github.com>
2022-03-30 00:48:22 +0100

    Show all products + one product by ID
    MmobileProduitController

--- src/Controller/MobileProduitController.php
+++ src/Controller/MobileProduitController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Produit;
 use App\Form\ProduitType;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 
 
 class MobileProduitController extends AbstractController
@@ -22,14 +24,22 @@ class MobileProduitController extends AbstractController
         ]);
     }
     /**
-     * @Route ("/allProduits", name="allProduits")
+     * @Route ("/allProd", name="allProduits")
      */
     public function AllProduits(NormalizerInterface $normalizer){
-        $repository=$this->getDoctrine()->getRepository(Produit::class);
-        $produits=$repository->findAll();
-        $jsonContent=$normalizer->normalize($produits, 'json',['groups'=>'post::read']);
-        return $this->render('mobile_produit/allProduits.html.twig',[
-            'data'=>$jsonContent,
-        ]);
+        $em = $this->getDoctrine()->getManager();
+        $produit=$em->getRepository(Produit::class)->findAll();
+        $jsonContent=$normalizer->normalize($produit,'json',['groups'=>'post:read']);
+        return new Response(json_encode($jsonContent));
+
+    }
+    /**
+     * @Route ("/allProduits/{id}", name="allProduits")
+     */
+    public function ProduitId(Request $request,$id,NormalizerInterface $normalizer){
+        $em = $this->getDoctrine()->getManager();
+        $produit=$em->getRepository(Produit::class)->find($id);
+        $jsonContent=$normalizer->normalize($produit,'json',['groups'=>'post:read']);
+        return new Response(json_encode($jsonContent));
     }
 }
