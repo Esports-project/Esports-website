145af20b61c007c9cd560f42ecc1a44f59ecf65d
RayenBourguiba <58385581+RayenBourguiba@users.noreply.github.com>
2022-03-31 04:52:08 +0100

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -43,4 +43,43 @@ class SecurityController extends AbstractController
         return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+
+    ////////////////////////////////json login ////////////////////
+    ///
+    ///
+/*
+    /**
+     *
+     * @Route ("signinJSON" , name ="app_login_JSON" )
+     */
+/*
+    public  function signinActionJSON(Request  $request){
+
+        $usernamel = $request->query->get("username");
+        $passwordl = $request->query->get("password");
+
+        $em=$this->getDoctrine()->getManager();
+        $utilisateur = $em->getRepository(Utilisateur::class)->findOneBy(['username'=>$usernamel]);
+
+        if($utilisateur){
+            if(password_verify($passwordl,$utilisateur->getPassword())){
+                $serializer = new Serializer([new ObjectNormalizer()]);
+                $formatted =$serializer->normalize($utilisateur);
+                return new JsonResponse($formatted);
+            }
+            else{
+                return  new Response("Password incorrect");
+            }
+
+        }
+        else{
+            return  new Response("Username incorrect");
+        }
+
+
+    }
+
+    */
+
 }
